91b4a63f9ddd2f2d551a84cfd6e664445dadb27c
HernanIruegas <hernaniruegas@hotmail.com>
2019-03-26 01:04:11 -0600

    lexer and parser

---
+++ lexer.py
@@ -0,0 +1,141 @@
+import ply.lex as lex
+
+# Reserved Keywords (there should be no grammatical rules for these)
+
+reserved = {
+    
+    # Code Related and Decision Making
+    'program' : 'program',
+    'start' : 'start',
+    'var' : 'var',
+    'scan' : 'scan',
+    'print': 'print',
+    'func' : 'func',
+    'while' : 'while',
+    'return' : 'return',
+    'if' : 'if',
+    'else' : 'else',
+    'void' : 'void',
+    'func' : 'func',
+    'return' : 'return',
+    'not' : 'not',
+    'and' : 'and',
+    'or' : 'or',
+
+    # Data Types
+    'int' : 'int',
+    'float' : 'float',
+    'string' : 'string',
+    'bool' : 'bool',
+    'TRUE' : 'TRUE',
+    'FALSE' : 'FALSE',
+
+    #Special Functions
+    'ols' : 'ols',
+    'las' : 'las',
+    'rid' : 'rid',
+    'kmeans' : 'kmeans',
+    'mbm' : 'mbm',
+    'tseries' : 'tseries',
+    'mean_abs_err' : 'mean_abs_err',
+    'mean_sqr_err' : 'mean_sqr_err',
+    'median_abs_err' : 'median_abs_err',
+    'mean' : 'mean',
+    'mode' : 'mode',
+    'median' : 'median',
+    'prob' : 'prob',
+    'freq' : 'freq',
+    'variance' : 'variance',
+    'stddev' : 'stddev',
+    'skew' : 'skew',
+    'kurt' : 'kurt'
+
+ }
+
+# Token List
+
+tokens = [
+    'id',
+    'semicolon',
+    'colon',
+    'comma',
+    'lSqrBracket',
+    'rSqrBracket',
+    'greater',
+    'lessThan',
+    'greaterEquals',
+    'lessThanEquals',
+    'equals',
+    'notEquals',
+    'minus',
+    'times',
+    'divide',
+    'plus',
+    'lParenthesis',
+    'rParenthesis',
+    'lCurlyBracket',
+    'rCurlyBracket',
+    'cte_str',
+    'cte_i',
+    'cte_f'
+] + list(reserved.values())
+
+
+# Regular Expressions for simple tokens (no action needed)
+
+t_semicolon = r'\;'
+t_colon = r'\:'
+t_comma = r'\,'
+t_lSqrBracket = r'\['
+t_rSqrBracket = r'\]'
+t_greater = r'>'
+t_lessThan = r'<'
+t_greaterEquals = r'>='
+t_lessThanEquals = r'<='
+t_equals = r'='
+t_notEquals = r'!='
+t_minus = r'-'
+t_times = r'\*'
+t_divide = r'/'
+t_plus = r'\+'
+t_lParenthesis = r'\('
+t_rParenthesis = r'\)'
+t_lCurlyBracket = r'\{'
+t_rCurlyBracket = r'\}'
+
+
+# Token rules specified as a functions (action needed)
+
+def t_id(t):
+    r"[A-Za-z_][A-Za-z0-9_]*"
+    t.type = reserved.get(t.value, "id")
+    return t
+
+def t_cte_str(t):
+    r'\".*\"'
+    t.value = str(t.value)
+    return t
+
+def t_cte_i(t):
+    r'[0-9]+'
+    t.value = int(t.value)
+    return t
+
+def t_cte_f(t):
+    r'[0-9]+\.[0-9]+'
+    t.value = float(t.value)
+    return t
+
+
+#t_ignore = " \t"
+#def t_newline(t):
+#    r"\n+"
+#    t.lexer.lineno += t.value.count("\n")
+
+# Error handling rule
+def t_error(t):
+ print("Illegal character '%s'" % t.value[0])
+ t.lexer.skip(1)
+
+# Build the lexer
+lexer = lex.lex()
---
+++ parser.py
@@ -0,0 +1,471 @@
+import ply.yacc as yacc
+from lexer import lexer, tokens
+
+# NumConst puro numero
+# VarConstAux puro numero y acceder a arreglo
+# checar si NumConst es necesario, porque segun yo se puede hacer con varcteaux (porque tambien se puede definir tamaños de arreglos si pasas el indice de un arreglo)
+
+def p_PROGRAM(p):
+	"""
+	PROGRAM : program id semicolon PROGRAM_A start BLOCK
+	"""
+
+def p_PROGRAM_A(p):
+	"""
+	PROGRAM_A : VARS PROGRAM_A
+			| METHOD PROGRAM_A
+			| empty
+	"""
+
+# falta poner la recursividad
+def p_VARS(p):
+	"""
+	VARS : var TYPE colon VARS_A semicolon
+	"""
+
+def p_VARS_A(p):
+	"""
+	VARS_A : SIMPLE
+		| LIST
+	"""
+
+def p_SIMPLE(p):
+	"""
+	SIMPLE : id SIMPLE_A
+	"""
+
+def p_SIMPLE_A(p):
+	"""
+	SIMPLE_A : comma SIMPLE
+			| empty
+	"""
+
+def p_LIST(p):
+	"""
+	LIST : id lSqrBracket NUMCONST rSqrBracket LIST_A
+	"""
+
+def p_LIST_A(p):
+	"""
+	LIST_A : LIST
+		| empty
+	"""
+
+# Para definir el tamaño de un arreglo
+# Solo se aceptan numeros 
+def p_NUMCONST(p):
+	"""
+	NUMCONST : id 
+			| cte_i 
+			| cte_f
+	"""
+
+def p_EXPLOG(p):
+	"""
+	EXPLOG : EXPRESSION EXPLOG_A
+		| not EXPRESSION EXPLOG_A
+	"""
+
+def p_EXPLOG_A(p):
+	"""
+	EXPLOG_A : and EXPLOG
+		| or EXPLOG
+		| empty
+	"""
+
+def p_EXPRESSION(p):
+	"""
+	EXPRESSION : EXP EXPRESSION_A EXP
+	"""
+
+def p_EXPRESSION_A(p):
+	"""
+	EXPRESSION_A : greater 
+				| lessThan
+				| greaterEquals
+				| lessThanEquals
+				| equals
+				| notEquals
+	"""
+
+def p_EXP(p):
+	"""
+	EXP : TERM EXP_A
+	"""
+
+def p_EXP_A(p):
+	"""
+	EXP_A : plus EXP
+		| minus EXP
+		| empty
+	"""
+
+def p_TERM(p):
+	"""
+	TERM : FACTOR TERM_A
+	"""
+
+def p_TERM_A(p):
+	"""
+	TERM_A : times TERM
+			| divide TERM
+			| empty
+	"""
+
+def p_FACTOR(p):
+	"""
+	FACTOR : lParenthesis EXPLOG rParenthesis
+			| VARCONSTAUX
+	"""
+
+# Define numeros y accesos a indices de arreglos (sin string y boolean de VARCTE)
+def p_VARCONSTAUX(p):
+	"""
+	VARCONSTAUX : id ISLIST 
+		| cte_i 
+		| cte_f 
+	"""
+
+def p_TYPE(p):
+	"""
+	TYPE : int
+		| float 
+		| string 
+		| bool
+	"""
+
+def p_BLOCK(p):
+	"""
+	BLOCK : lCurlyBracket BLOCK_A rCurlyBracket
+	"""
+
+def p_BLOCK_A(p):
+	"""
+	BLOCK_A : STATEMENT
+			| empty
+	"""
+
+def p_STATEMENT(p):
+	"""
+	STATEMENT : ASSIGNMENT
+			| CONDITION
+			| WRITE
+			| LOOP
+			| METHODCALL
+			| READ
+			| STATMETHODS
+			| RETURN 
+	"""
+
+def p_ASSIGNMENT(p):
+	"""
+	ASSIGNMENT : id ISLIST equals EXPRESSION semicolon 
+	"""
+
+def p_READ(p):
+	"""
+	READ : scan lParenthesis VARCTE READ_A rParenthesis
+	"""
+
+def p_READ_A(p):
+	"""
+	READ_A : comma
+		| empty
+	"""
+
+def p_ISLIST(p):
+	"""
+	ISLIST : lSqrBracket EXP rSqrBracket
+			| empty 
+	"""
+
+def p_TYPEMETHOD(p):
+	"""
+	TYPEMETHOD : TYPE
+			| void 
+	"""
+
+def p_CONDITION(p):
+	"""
+	CONDITION : if lParenthesis EXPLOG rParenthesis BLOCK CONDITION_A semicolon
+	"""
+
+def p_CONDITION_A(p):
+	"""
+	CONDITION_A : else BLOCK
+				| empty
+	"""
+
+def p_WRITE(p):
+	"""
+	WRITE : print lParenthesis EXPRESSION WRITE_A rParenthesis semicolon
+	"""
+
+def p_WRITE_A(p):
+	"""
+	WRITE_A : comma EXPRESSION WRITE_A
+		| empty
+	"""
+
+def p_VARCTE(p):
+	"""
+	VARCTE : id ISLIST 
+		| cte_i 
+		| cte_f 
+		| cte_str 
+		| BOOLEAN
+	"""
+
+def p_METHOD(p):
+	"""
+	METHOD : func TYPEMETHOD id lParenthesis PARAMS rParenthesis BLOCK semicolon
+	"""
+
+def p_PARAMS(p):
+	"""
+	PARAMS : EXP PARAMS_A
+	"""
+
+def p_PARAMS_A(p):
+	"""
+	PARAMS_A : comma EXP PARAMS_A
+		| empty
+	"""
+
+def p_LOOP(p):
+	"""
+	LOOP : while lParenthesis EXPLOG rParenthesis BLOCK
+	"""
+
+def p_METHODCALL(p):
+	"""
+	METHODCALL : id lParenthesis EXP METHODCALL_A rParenthesis semicolon
+	"""
+
+def p_METHODCALL_A(p):
+	"""
+	METHODCALL_A : comma EXP
+				| empty
+	"""
+
+def p_RETURN(p):
+	"""
+	RETURN : return EXPLOG semicolon
+	"""
+
+def p_BOOLEAN(p):
+	"""
+	BOOLEAN : FALSE
+			| TRUE
+	"""
+
+def p_STATMETHODS(p):
+	"""
+	STATMETHODS : ORDINARY_LEAST_SQUARES
+				| LASSO
+				| RIDGE
+				| K_MEANS
+				| MINI_BATCH_MEANS
+				| TIME_SERIES_SPLIT
+				| MEAN_ABSOLUTE_ERROR
+				| MEAN_SQUARED_ERROR
+				| MEDIAN_ABSOLUTE_ERROR
+				| MEAN
+				| MODE
+				| MEDIAN
+				| PROBABILITY
+				| FREQUENCY
+				| VARIANCE
+				| STANDARD_DEVIATION
+				| SKEWNESS
+				| KURTOSI
+	"""
+
+def p_ORDINARY_LEAST_SQUARES(p):
+	"""
+	ORDINARY_LEAST_SQUARES : ols lParenthesis id comma id comma BOOLEAN comma BOOLEAN comma VARCONSTAUX comma BOOLEAN rParenthesis semicolon
+	"""
+
+def p_LASSO(p):
+	"""
+	LASSO : las lParenthesis id comma id comma VARCONSTAUX comma BOOLEAN comma BOOLEAN comma VARCONSTAUX comma BOOLEAN comma BOOLEAN comma BOOLEAN comma VARCONSTAUX comma string comma float comma BOOLEAN rParenthesis semicolon
+	"""
+
+def p_RIDGE(p):
+	"""
+	RIDGE : rid lParenthesis id comma id comma VARCONSTAUX comma BOOLEAN comma BOOLEAN comma BOOLEAN comma VARCONSTAUX comma VARCONSTAUX comma string comma VARCONSTAUX rParenthesis semicolon
+	"""
+
+def p_K_MEANS(p):
+	"""
+	K_MEANS : kmeans lParenthesis VARCONSTAUX comma string comma VARCONSTAUX comma VARCONSTAUX comma VARCONSTAUX comma string comma VARCONSTAUX comma VARCONSTAUX comma BOOLEAN comma VARCONSTAUX comma string rParenthesis semicolon
+	"""
+
+def p_MINI_BATCH_MEANS(p):
+	"""
+	MINI_BATCH_MEANS : mbm lParenthesis VARCONSTAUX comma string comma VARCONSTAUX comma VARCONSTAUX comma BOOLEAN comma BOOLEAN comma VARCONSTAUX comma VARCONSTAUX comma VARCONSTAUX comma VARCONSTAUX comma VARCONSTAUX comma VARCONSTAUX rParenthesis semicolon 
+	"""
+
+def p_TIME_SERIES_SPLIT(p):
+	"""
+	TIME_SERIES_SPLIT : tseries lParenthesis VARCONSTAUX comma VARCONSTAUX rParenthesis semicolon 
+	"""
+
+def p_MEAN_ABSOLUTE_ERROR(p):
+	"""
+	MEAN_ABSOLUTE_ERROR : mean_abs_err lParenthesis id comma id rParenthesis semicolon
+	"""
+
+def p_MEAN_SQUARED_ERROR(p):
+	"""
+	MEAN_SQUARED_ERROR : mean_sqr_err lParenthesis id comma id rParenthesis semicolon 
+	"""
+
+def p_MEDIAN_ABSOLUTE_ERROR(p):
+	"""
+	MEDIAN_ABSOLUTE_ERROR : median_abs_err lParenthesis id comma id rParenthesis semicolon 
+	"""
+
+def p_MEAN(p):
+	"""
+	MEAN : mean lParenthesis id MEAN_A rParenthesis semicolon
+	"""
+
+def p_MEAN_A(p):
+	"""
+	MEAN_A : comma id MEAN_A
+		| empty
+	"""
+
+def p_MODE(p):
+	"""
+	MODE : mode lParenthesis id MODE_A rParenthesis semicolon
+	"""
+
+def p_MODE_A(p):
+	"""
+	MODE_A : comma id MODE_A
+		| empty
+	"""
+
+def p_MEDIAN(p):
+	"""
+	MEDIAN : median lParenthesis id MEDIAN_A rParenthesis semicolon
+	"""
+
+def p_MEDIAN_A(p):
+	"""
+	MEDIAN_A : comma id MEDIAN_A
+		| empty
+	"""
+
+def p_PROBABILITY(p):
+	"""
+	PROBABILITY : prob lParenthesis id PROBABILITY_A rParenthesis semicolon
+	"""
+
+def p_PROBABILITY_A(p):
+	"""
+	PROBABILITY_A : comma id PROBABILITY_A
+		| empty
+	"""
+
+def p_FREQUENCY(p):
+	"""
+	FREQUENCY : freq lParenthesis id FREQUENCY_A rParenthesis semicolon
+	"""
+
+def p_FREQUENCY_A(p):
+	"""
+	FREQUENCY_A : comma id FREQUENCY_A
+		| empty
+	"""
+
+def p_VARIANCE(p):
+	"""
+	VARIANCE : variance lParenthesis id VARIANCE_A rParenthesis semicolon
+	"""
+
+def p_VARIANCE_A(p):
+	"""
+	VARIANCE_A : comma id VARIANCE_A
+		| empty
+	"""
+
+def p_STANDARD_DEVIATION(p):
+	"""
+	STANDARD_DEVIATION : stddev lParenthesis id STANDARD_DEVIATION_A rParenthesis semicolon
+	"""
+
+def p_STANDARD_DEVIATION_A(p):
+	"""
+	STANDARD_DEVIATION_A : comma id STANDARD_DEVIATION_A
+		| empty
+	"""
+
+def p_SKEWNESS(p):
+	"""
+	SKEWNESS : skew lParenthesis id SKEWNESS_A rParenthesis semicolon
+	"""
+
+def p_SKEWNESS_A(p):
+	"""
+	SKEWNESS_A : comma id SKEWNESS_A
+		| empty
+	"""
+
+def p_KURTOSI(p):
+	"""
+	KURTOSI : kurt lParenthesis id KURTOSI_A rParenthesis semicolon
+	"""
+
+def p_KURTOSI_A(p):
+	"""
+	KURTOSI_A : comma id KURTOSI_A
+		| empty
+	"""
+
+def p_empty(p):
+    """
+    empty :
+    """
+
+def p_error(p):
+    print("Syntax error at '%s'" % p)
+    exit(1)
+
+parser = yacc.yacc()
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
